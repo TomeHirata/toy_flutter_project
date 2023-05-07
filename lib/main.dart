3c71db879b4d4a19d6b34229a4ec6b09b2d6f06e
TomeHirata <tomu.hirata@gmail.com>
2023-05-08 01:19:21 +0900

    feat: add chatGPT tab

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +20,7 @@ class MyApp extends StatelessWidget {
         title: 'First Flutter Application',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         ),
         home: MyHomePage(),
       ),
@@ -25,8 +28,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
+enum UserType {
+  chatGPT,
+  user,
+} 
+
+class Message {
+  UserType userType;
+  String content;
+  Message(this.userType, this.content);
+}
+
+var openaiKey = "YOUR KEY";
+
+Future<String> fetchReply(String prompt) async {
+  final response = await http
+      .post(
+        Uri.parse('https://api.openai.com/v1/chat/completions'),
+        headers: { 
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $openaiKey',
+        },
+        body: jsonEncode({
+          'model': "gpt-3.5-turbo-0301",
+          "messages": [{"role": "user", "content": prompt}]
+        })
+      );
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    // todo: make it a struct
+    final data = jsonDecode(response.body);
+    return data["choices"][0]["message"]["content"];
+  } else {
+    print(response.body);
+    print(response.statusCode);
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load reply');
+  }
+}
+
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var currentPrompt = "";
+  var conversation = <Message>[];
 
   void getNext() {
     current = WordPair.random();
@@ -41,6 +88,20 @@ class MyAppState extends ChangeNotifier {
     } else {
       favorites.add(current);
     }
+    notifyListeners();
+  }
+
+  void setPrompt(String prompt) {
+    currentPrompt = prompt;
+  }
+
+  void initPrompt() {
+    currentPrompt = "";
+    notifyListeners();
+  }
+
+  void addMessage(Message message) {
+    conversation.add(message);
     notifyListeners();
   }
 }
@@ -63,9 +124,14 @@ class _MyHomePageState extends State<MyHomePage> {
       case 1:
         page = FavoritesPage();
         break;
+      case 2:
+        page = ChatPage();
+        break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
+    var theme = Theme.of(context);
+    
     return LayoutBuilder(
       builder: (context, constraints) {
         return Scaffold(
@@ -73,6 +139,9 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               SafeArea(
                 child: NavigationRail(
+                  indicatorColor: theme.colorScheme.secondary,
+                  unselectedLabelTextStyle: const TextStyle(color: Colors.white),
+                  selectedLabelTextStyle: TextStyle(color: Colors.white, backgroundColor: theme.focusColor),
                   extended: constraints.maxWidth >= 600,
                   destinations: [
                     NavigationRailDestination(
@@ -83,6 +152,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       icon: Icon(Icons.favorite),
                       label: Text('Favorites'),
                     ),
+                    NavigationRailDestination(
+                      icon: FaIcon(FontAwesomeIcons.robot),
+                      label: Text('ChatGPT'),
+                    ),
                   ],
                   selectedIndex: selectedIndex,
                   onDestinationSelected: (value) {
@@ -90,11 +163,12 @@ class _MyHomePageState extends State<MyHomePage> {
                       selectedIndex = value;
                     });
                   },
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
                 ),
               ),
               Expanded(
                 child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
+                  color: Theme.of(context).colorScheme.background,
                   child: page,
                 ),
               ),
@@ -111,25 +185,94 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
+    return Center(
+      child: Column(
+        children: [
+          ListView(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text('You have '
+                    '${appState.favorites.length} favorites:'),
+              ),
+              for (var pair in appState.favorites)
+                ListTile(
+                  leading: Icon(Icons.favorite),
+                  title: Text(pair.asLowerCase),
+                ),
+            ],
           ),
-      ],
+        ],
+      ),
+    );
+  }
+}
+
+class ChatPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Center(
+      child: Stack(
+        children: [
+          Container(
+            clipBehavior: Clip.none,
+            alignment: Alignment.topCenter,
+            margin: const EdgeInsets.only(bottom: 80), // todo: align with button height
+            child: ListView(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                ),
+                for (var message in appState.conversation)
+                  Material(
+                    child: ListTile(
+                      tileColor: message.userType == UserType.chatGPT ? Color.fromARGB(255, 201, 201, 201) : Colors.white,
+                      leading: message.userType == UserType.chatGPT ? (FaIcon(FontAwesomeIcons.robot)) : FaIcon(FontAwesomeIcons.user),
+                      title: Text(message.content),
+                    ),
+                  )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Send a message',
+                      ),
+                      onChanged: (text) {
+                        appState.setPrompt(text);
+                      },
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    appState.addMessage(Message(UserType.user, appState.currentPrompt));
+          
+                    final res = await fetchReply(appState.currentPrompt);
+                    appState.addMessage(Message(UserType.chatGPT, res));
+                    appState.initPrompt();
+                  },
+                  child: Icon(Icons.send),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
